313fd86c6ac5149c107915c86f884dc4a54af6f7
Melonguy <35050861+Xc2333@users.noreply.github.com>
2018-12-28 22:16:36 +0800

    fix Version bugs

--- AppleALC-Generator.sh
+++ AppleALC-Generator.sh
@@ -18,6 +18,8 @@ download="svn checkout"
 
 ## download lilu.kext (debug version)
     cd ~/Desktop && mkdir AppleAlcCustom && cd AppleAlcCustom
+    curl -O https://raw.githubusercontent.com/acidanthera/AppleALC/master/Changelog.md
+    AppleALCVer=`grep "###"  *.md|head -1|awk '{print $2}'`
     curl -L -# https://github.com/acidanthera/Lilu/releases -o lilu
     DebugLiluVer=`grep tree lilu |head -n 1|awk -F'title="' '{print $2}'|awk -F'"' '{print $1}'`
     curl -L -O -# https://github.com/acidanthera/Lilu/releases/download/$DebugLiluVer/$DebugLiluVer.DEBUG.zip
@@ -34,10 +36,10 @@ download="svn checkout"
      xcodebuild -project AppleALC.xcodeproj -configuration Release
 
 ## cp
-    cp -R  build/Release/AppleALC.kext ~/Desktop/AppleALC_CX20751_v$DebugLiluVer.kext
+    cp -R  build/Release/AppleALC.kext ~/Desktop/AppleALC_CX20751_$AppleALCVer.kext
 
 ## done.  assuming no error  -_-||
-    echo  "\033[1;32m AppleALC_CX20751_v$DebugLiluVer.kext has been built. \033[0m"
+    echo  "\033[1;32m AppleALC_CX20751_v$AppleALCVer.kext has been built. \033[0m"
 
 ## clean
     rm lilu $DebugLiluVer.DEBUG.zip
